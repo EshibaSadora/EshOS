07229445dd32f67641e86643a004dbfd8c59bd41
dantDuc <egorshornikov@mail.ru>
2018-09-14 08:29:08 +0400

    EshOS libs

---
+++ EshOS/EshConsoleOS.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Esh.ConsoleOS
+{
+    static public class OS
+    {
+        static bool sist = false;
+
+        static void menu(int pos,string label,string [] buttons,ConsoleColor col)
+        {
+            //Console.Clear();
+
+            Console.WriteLine(label);
+
+            for (int i = 1; i < buttons.Length; i++)
+            {
+                Console.BackgroundColor = ConsoleColor.Black; Console.ForegroundColor = ConsoleColor.White;
+                if (pos == i) { Console.BackgroundColor = ConsoleColor.White; Console.ForegroundColor = ConsoleColor.Black; }
+                Console.WriteLine(buttons[i]);
+            }
+
+
+          //  Console.BackgroundColor = ConsoleColor.Black; Console.ForegroundColor = ConsoleColor.White;
+        }
+        static void menu(int pos, string[] buttons, ConsoleColor col)
+        {
+            //Console.Clear();
+
+            for (int i = 1; i < buttons.Length; i++)
+            {
+                Console.BackgroundColor = ConsoleColor.Black; Console.ForegroundColor = ConsoleColor.White;
+                if (pos == i) { Console.BackgroundColor = ConsoleColor.White; Console.ForegroundColor = ConsoleColor.Black; }
+                Console.WriteLine(buttons[i]);
+            }
+
+
+            //  Console.BackgroundColor = ConsoleColor.Black; Console.ForegroundColor = ConsoleColor.White;
+        }
+        static void menu(int pos, string[] buttons)
+        {
+            //Console.Clear();
+
+            for (int i = 1; i < buttons.Length; i++)
+            {
+                Console.BackgroundColor = ConsoleColor.Black; Console.ForegroundColor = ConsoleColor.White;
+                if (pos == i) { Console.BackgroundColor = ConsoleColor.White; Console.ForegroundColor = ConsoleColor.Black; }
+                Console.WriteLine(buttons[i-1]);
+            }
+              
+        }
+
+        static public void drawline()
+        {
+            // Console.WriteLine("");
+            for (int i = 0; i < Console.WindowWidth; i++)
+            {
+                Console.Write("-");
+            }
+            //  Console.WriteLine("");
+        }
+
+        static public void drawline(char a)
+        {
+            // Console.WriteLine("");
+            for (int i = 0; i < Console.WindowWidth; i++)
+            {
+                Console.Write(a);
+            }
+            //  Console.WriteLine("");
+        }
+
+        static int cursor(int menupoz, string[] buttons)
+        {
+            ConsoleKeyInfo cki;
+
+
+            cki = Console.ReadKey(true);
+
+            if (cki.Key == ConsoleKey.DownArrow) { menupoz++; }
+            if (cki.Key == ConsoleKey.UpArrow) { menupoz--; }
+
+
+            if (menupoz > buttons.Length) { menupoz = 1; }
+            if (menupoz < 1) { menupoz = 1; }
+
+            if (cki.Key == ConsoleKey.Enter) { sist = true; }
+
+
+
+            Console.Clear();
+            return menupoz;
+
+        }
+
+        static public int form(string[] buttons)
+        {
+         //   string lbl = "Новая форма";
+            string mess = "";
+
+            bool loggin = false;
+            int pos = 1;
+
+            while (loggin == false)
+            {
+                Console.BackgroundColor = ConsoleColor.Black; Console.ForegroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                drawline('*');
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(mess);
+                Console.WriteLine();
+                menu(pos, buttons);
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                drawline('*');
+                pos = cursor(pos, buttons);
+                if (pos == buttons.Length + 2) { return pos-2; };
+            }
+
+            return -1;
+        }
+        static public int form(string[] buttons,string _lbl)
+        {
+            string lbl = _lbl;
+            string mess = "";
+
+            bool loggin = false;
+            int pos = 1;
+
+            while (loggin == false)
+            {
+                Console.BackgroundColor = ConsoleColor.Black; Console.ForegroundColor = ConsoleColor.White;
+                // buttons = new string[] { "Вход", "Регистрация", "" };
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                drawline('*');
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(mess);
+                Console.WriteLine();
+                menu(pos, buttons);
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                drawline('*');
+                pos = cursor(pos, buttons);
+                if (pos == buttons.Length + 2) { return pos - 2; };
+            }
+
+            return -1;
+        }
+        static public int form(string[] buttons, string _lbl, string _mess)
+        {
+            string lbl = _lbl;
+            string mess = _mess;
+
+            bool loggin = false;
+            int pos = 1;
+
+            while (loggin == false)
+            {
+                Console.BackgroundColor = ConsoleColor.Black; Console.ForegroundColor = ConsoleColor.White;
+                // buttons = new string[] { "Вход", "Регистрация", "" };
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                drawline('*');
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(mess);
+                Console.WriteLine();
+                menu(pos, buttons);
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                drawline('*');
+                pos = cursor(pos, buttons);
+                if (sist == true) { sist = false;  return pos; };
+            }
+
+            return -1;
+        }
+
+        static public void AnyKey()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Нажмите любую кнопку");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EshOS/EshForms.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+using System.IO;
+
+namespace Esh.Forms
+{
+    public class Filedialog
+    {
+        /// <summary>
+        /// Возвращает строковое значение пути до файла, который нужно загрузить
+        /// <param name="filetype">filetype - Укажите расширение файла</param>
+        /// </summary>
+        public string openfile(string filetype)
+        {
+            string file = null;
+            Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            openFileDialog1.Filter = filetype + " files (*." + filetype + ")|*." + filetype + "|All files (*.*)|*.*";
+            // openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            //Код чтения
+                            file = openFileDialog1.FileName;
+                            myStream.Close();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Невозможно прочитать файл  " + ex.ToString());
+
+                }
+            }
+
+            return file;
+        }
+
+        /// <summary>
+        /// Возвращает строковое значение пути до файла, который нужно сохранить.
+        /// Создаёт пустой файл.
+        /// <param name="filetype">filetype - Укажите расширение файла</param>
+        /// </summary>
+        public string savefile(string filetype)
+        {
+            string dir = null;
+
+            Stream myStream;
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            saveFileDialog1.Filter = filetype + " files (*." + filetype + ")|*." + filetype + "|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if ((myStream = saveFileDialog1.OpenFile()) != null)
+                {
+                    // Code to write the stream goes here.
+                    dir = saveFileDialog1.FileName;
+
+                    myStream.Close();
+                }
+            }
+
+            return dir;
+        }
+    }
+
+    public static class Msg
+    {
+        public static void Show()
+        {
+            MessageBox.Show("Тут");
+        }
+
+        public static void Show(int a)
+        {
+            MessageBox.Show(a.ToString());
+        }
+
+        public static void Show(string a)
+        {
+            MessageBox.Show(a);
+        }
+
+        public static void Show(float a)
+        {
+            MessageBox.Show(a.ToString());
+        }
+
+        public static void Show(decimal a)
+        {
+            MessageBox.Show(a.ToString());
+        }
+
+    }
+}
+
