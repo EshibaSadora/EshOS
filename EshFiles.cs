870590da7cd10d6020c0f00a4a43e6265e266d80
dantDuc <egorshornikov@mail.ru>
2018-10-31 08:28:07 +0300

    File lib
    eazy work with txt

---
+++ EshFiles.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.IO;
+
+namespace Esh
+{
+    public class Files
+    {
+
+        string path;
+        string[] pathes;
+
+        Files(string _path)
+        {
+            path = _path;
+        }
+
+        Files(string [] _path)
+        {
+            pathes = _path;
+        }
+
+        Files()
+        {
+
+        }
+
+        public string readtxt()
+        {
+            string str = "";
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    str += line + "\n";
+                }
+            }
+            return str;
+        }
+
+        public void writetxt(string str)
+        {
+            using (StreamWriter sr = new StreamWriter(path))
+            {
+                sr.Write(str);
+            }
+        }
+
+        public static void writetxt(string path, string str)
+        {
+            using (StreamWriter sr = new StreamWriter(path))
+            {
+                sr.Write(str);
+            }
+        }
+
+        public static void WritelineTotxt(string path, string str)
+        {
+            using (StreamWriter sr = new StreamWriter(path))
+            {
+                sr.WriteLine(str);
+            }
+        }
+    }
+}
